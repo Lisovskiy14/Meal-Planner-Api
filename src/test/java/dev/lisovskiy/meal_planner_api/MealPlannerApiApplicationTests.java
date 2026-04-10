65237364c2cb43bc9806c87deb40ed6bc309c38a
Lisovskiy14 <nazarlisovskij2@gmail.com>
2026-04-10 13:41:21 +0300

    init

---
+++ src/test/java/dev/lisovskiy/meal_planner_api/MealPlannerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lisovskiy.meal_planner_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealPlannerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
