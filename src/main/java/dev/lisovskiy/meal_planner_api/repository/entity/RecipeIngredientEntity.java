5450e47999d8ad8cc8d191808e7e365d865ffd7e
Lisovskiy14 <nazarlisovskij2@gmail.com>
2026-05-12 23:46:59 +0300

    Add RecipeIngredientEntity

---
+++ src/main/java/dev/lisovskiy/meal_planner_api/repository/entity/RecipeIngredientEntity.java
@@ -0,0 +1,32 @@
+package dev.lisovskiy.meal_planner_api.repository.entity;
+
+import dev.lisovskiy.meal_planner_api.common.IngredientUnit;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "recipe_ingredients")
+@Getter
+@Setter
+public class RecipeIngredientEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "recipe_ingredient_seq_gen")
+    @SequenceGenerator(
+            name = "recipe_ingredient_seq_gen",
+            sequenceName = "recipe_ingredient_seq"
+    )
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "ingredient_id",  nullable = false)
+    private IngredientEntity ingredient;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "unit", nullable = false)
+    private IngredientUnit unit;
+
+    @Column(name = "quantity")
+    private Double quantity;
+}
