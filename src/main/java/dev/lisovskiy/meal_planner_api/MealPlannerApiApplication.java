8fe99b2c30eeaae148cea11a52cf5c85be8b5bfb
Lisovskiy14 <nazarlisovskij2@gmail.com>
2026-05-20 02:17:21 +0300

    Add '.env exclusion in .gitignore

--- src/main/java/dev/lisovskiy/meal_planner_api/MealPlannerApiApplication.java
+++ src/main/java/dev/lisovskiy/meal_planner_api/MealPlannerApiApplication.java
@@ -3,10 +3,13 @@ package dev.lisovskiy.meal_planner_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class MealPlannerApiApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		SpringApplication.run(MealPlannerApiApplication.class, args);
 	}
 
